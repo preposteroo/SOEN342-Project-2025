777a5f1232fbfbbb593ff72e156ef64e60416120
brandon-tol <brandon@thetoledanos.com>
2024-10-18 18:03:55 -0400

    Add DAOs

---
+++ coursecompass/src/main/java/com/soen342HB/coursecompass/spaces/LocationDAO.java
@@ -0,0 +1,48 @@
+package com.soen342HB.coursecompass.spaces;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.soen342HB.coursecompass.core.IDAO;
+
+public class LocationDAO implements IDAO<Location> {
+    // temp DB
+    public static List<Location> db = new ArrayList<Location>();
+
+    @Override
+    public void addtoDb(Location location) {
+        db.add(location);
+    }
+
+    @Override
+    public void removeFromDb(Location location) {
+        db.remove(location);
+    }
+
+    @Override
+    public Location fetchFromDb(String id) {
+        for (Location location : db) {
+            if (location.getLocationName().equals(id)) {
+                return location;
+            }
+        }
+        return null;
+    }
+
+    public Location[] fetchAllFromDb() {
+        Location[] locations = new Location[db.size()];
+        for (int i = 0; i < db.size(); i++) {
+            locations[i] = db.get(i);
+        }
+        return locations;
+    }
+
+    @Override
+    public void updateDb(Location location) {
+        for (Location l : db) {
+            if (l.getLocationName().equals(location.getLocationName())) {
+                l.setSpaces(location.getSpaces());
+            }
+        }
+    }
+
+}
---
+++ coursecompass/src/main/java/com/soen342HB/coursecompass/spaces/SpaceDAO.java
@@ -0,0 +1,42 @@
+package com.soen342HB.coursecompass.spaces;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.soen342HB.coursecompass.core.IDAO;
+
+public class SpaceDAO implements IDAO<Space> {
+    // temp DB
+    public static List<Space> db = new ArrayList<Space>();
+
+    @Override
+    public void addtoDb(Space space) {
+        db.add(space);
+    }
+
+    @Override
+    public void removeFromDb(Space space) {
+        db.remove(space);
+    }
+
+    @Override
+    public Space fetchFromDb(String id) {
+        for (Space space : db) {
+            if (space.getSpaceName().equals(id)) {
+                return space;
+            }
+        }
+        return null;
+    }
+
+    public Space[] fetchAllFromDb() {
+        Space[] spaces = new Space[db.size()];
+        for (int i = 0; i < db.size(); i++) {
+            spaces[i] = db.get(i);
+        }
+        return spaces;
+    }
+
+    @Override
+    public void updateDb(Space space) {}
+
+}
