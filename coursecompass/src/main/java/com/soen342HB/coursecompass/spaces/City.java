36dba4f498b2c06cf0cf0a3e70dfc483945352d2
Hannah Refour-Tannenbaum <105230926+preposteroo@users.noreply.github.com>
2024-10-18 17:43:09 -0400

    initial commit of Location and City classes

---
+++ coursecompass/src/main/java/com/soen342HB/coursecompass/spaces/City.java
@@ -0,0 +1,30 @@
+package com.soen342HB.coursecompass.spaces;
+
+import java.util.ArrayList;
+
+public class City {
+    private String cityName;
+    private ArrayList<Location> locations;
+
+
+    public City(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public ArrayList<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(ArrayList<Location> locations) {
+        this.locations = locations;
+    }
+
+}
